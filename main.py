6fb8cbc37e058fe4268c4add6337851755da718d
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2018-11-13 13:49:59 +0900

    Add index rooting

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__    = 'Kazuyuki TAKASE'
+__copyright__ = 'PLEN Project Company Inc, and all authors.'
+__license__   = 'The MIT License (http://opensource.org/licenses/mit-license.php)'
+
+
 import os
 
 from bottle import Bottle, request
@@ -10,7 +17,7 @@ router = Bottle()
 server = WSGIServer(('0.0.0.0', int(os.environ.get('PORT', 5000))), router, handler_class=WebSocketHandler)
 
 
-@router.route('/')
+@router.route('/msg-bridge')
 def handle_websocket():
     websocket = request.environ.get('wsgi.websocket')
 
@@ -30,6 +37,11 @@ def handle_websocket():
 
         except WebSocketError:
             break
+
+
+@router.route('/')
+def handle_index():
+    return 'HTTP request is not supported on this server.'
 
 
 server.serve_forever()
